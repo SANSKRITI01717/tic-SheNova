4eacc247161f9986570ed4d321a38a4c18da1131
Somya Devda <devsomya@gmail.com>
2026-04-05 02:26:00 +0530

    made navbar responsive

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,9 @@ document.querySelectorAll('input[type="date"]').forEach(input => {
     input.max = today;
   }
 });
+const btn = document.getElementById('hamburger');
+const nav = document.getElementById('nav-links');
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open');
+  nav.classList.toggle('open');
+});
